70f8d17bd25226642f30c3962aedb5ee3e1c3ecf
The-Normal-One1 <manermidem@gmail.com>
2022-11-18 10:02:34 +0300

    Modify the files to add API for the project

--- src/index.js
+++ src/index.js
@@ -1,2 +1,50 @@
 // import _ from 'lodash';
-import './style.css';
+import "./style.css";
+import { submit, scoreList, refreshBtn } from "./modules/project.js";
+
+submit.addEventListener("click", async () => {
+  const userName = document.querySelector("#name");
+  const score = document.querySelector("#score");
+
+  const data = {
+    user: userName.value,
+    score: score.value,
+  };
+
+  try {
+    const response = await fetch(
+      `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${process.env.API_KEY}/scores/`,
+      {
+        method: "POST", // or 'PUT'
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    );
+    await response.json();
+    userName.value = "";
+    score.value = "";
+  } catch (error) {
+    throw Error(error);
+  }
+});
+
+// refreshBtn
+
+refreshBtn.addEventListener("click", async () => {
+  try {
+    const response = await fetch(
+      `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${process.env.API_KEY}/scores/`
+    );
+    const data = await response.json();
+    // refresh
+    data.result.forEach((item) => {
+      const scoreListItem = document.createElement("li");
+      scoreListItem.innerHTML = `${item.user}: ${item.score}`;
+      scoreList.append(scoreListItem);
+    });
+  } catch (error) {
+    throw Error(error);
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
 
 module.exports = {
   entry: './src/index.js',
@@ -10,6 +12,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
+    new Dotenv(),
   ],
   output: {
     filename: '[name].js',
